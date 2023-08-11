0bb5172dcd1ad26de254cae954329b1886e56a08
Victor <victortargino@gmail.com>
2023-08-11 11:38:40 -0300

    logos11.08.21

--- src/componentes/outros/redesSociais/socialMedia.js
+++ src/componentes/outros/redesSociais/socialMedia.js
@@ -6,22 +6,26 @@ function SocialMedia() {
          <ul>
             <li>
                <i className="fa-brands fa-instagram"/>
-               <span>instagram</span>
+               <a href="" rel="noreferrer" target="_blank">instagram</a>
             </li>
             
             <li>
                <i className="fa-brands fa-linkedin"/>
-               <span>linkedin</span>
+               <a href="" rel="noreferrer" target="_blank">linkedin</a>
             </li>
       
             <li>
                <i className="fa-brands fa-github"/>
-               <span>gitHub</span>
+               <a href="https://github.com/victortarg" rel="noreferrer" target="_blank">
+                  <img src="https://d33wubrfki0l68.cloudfront.net/38469cf88d038b6ba3322c9fcb93a8f7167df4b9/cb0b9/assets/svg/github-dark.svg" className="img_social_media"/>
+                  github
+               </a>
+
             </li>
       
             <li>
                <i className="fa-brands fa-google"/>
-               <span>gmail</span>
+               <a href="" rel="noreferrer" target="_blank">gmail</a>
             </li>
 
          </ul>
